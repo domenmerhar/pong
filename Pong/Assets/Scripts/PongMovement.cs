7f5f856de1fb1693a47ea76c8309b04128eeb8e7
domenmerhar <merhardomen@gmail.com>
2023-05-31 08:25:57 +0200

    Added Bracket Movement

--- Pong/Assets/Scripts/PongMovement.cs
+++ Pong/Assets/Scripts/PongMovement.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PongMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rigidBody;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float speed;
+
+    private void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+    private void OnEnable()
+    {
+        rigidBody.velocity = Vector3.up * speed;
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            rigidBody.velocity = Vector3.up * speed; 
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            rigidBody.velocity = Vector3.down * speed; 
+        }
     }
 }
